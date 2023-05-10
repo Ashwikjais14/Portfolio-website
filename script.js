93853c5c427ec9bbbd3040b350589bcecd0947e2
Ashwikjais14 <jaiswalashwik14feb@gmail.com>
2023-05-10 23:34:40 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+/======================typing animation=======/
+var typed = new Typed(".typing" ,{
+    strings: ["Web Designer","Web Developer","Graphics Designer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+
+})
